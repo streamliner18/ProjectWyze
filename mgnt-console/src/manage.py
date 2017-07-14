66e7a1c85f3582370a4547b4b8c663ed0139c984
Streamliner <jimmy.xrail@me.com>
2017-07-14 12:53:48 +0800

    [mgnt] Manage.py and initial seeding capabilities

---
+++ mgnt-console/src/manage.py
@@ -0,0 +1,16 @@
+from flask_script import Manager
+from app.app import app
+from app.services import seeds
+
+manager = Manager(app)
+
+
+@manager.command
+def init():
+    print('Initializing database. The old db will be wiped.')
+    seeds.build_database()
+    seeds.initial_database_seed()
+    print('Initialization complete. Create your admin account using invitation ID "root"')
+
+if __name__ == "__main__":
+    manager.run()
